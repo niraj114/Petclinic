986bde2caec74c672787a0a2fc84088b38cbb9bc
NIraj Kumar <niraj333aug@gmail.com>
2023-07-21 05:40:18 -0700

    First commit

---
+++ src/main/java/com/patclinic/patclinic/PatclinicApplication.java
@@ -0,0 +1,13 @@
+package com.patclinic.patclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patclinic/patclinic/PatclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patclinic.patclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
